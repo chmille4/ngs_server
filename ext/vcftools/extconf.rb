100967e1c418f5cf81de98eba82c518e742ca99f
Chase Miller <chmille4@gmail.com>
2011-12-12 07:21:15 -0500

    removed MakeFile from vcftools

--- ext/vcftools/extconf.rb
+++ ext/vcftools/extconf.rb
@@ -3,7 +3,22 @@
 
 #Dir.chdir("/Users/chase/Desktop/tmp_workspace/ngs_server/ext/bamtools/build")
 
-puts 'nothing to do'
+file = File.new('MakeFile', 'w')
+
+file. puts "ifndef PREFIX
+    export PREFIX = $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
+endif
+export BINDIR = ${PREFIX}/bin
+export MODDIR = ${PREFIX}/lib
+
+DIRS = cpp perl
+install:
+	    @mkdir -p $(BINDIR); mkdir -p $(MODDIR); \
+        for dir in $(DIRS); do cd $$dir && $(MAKE) $(MAKEFLAGS) && cd ..; done
+
+clean:
+		@for dir in $(DIRS); do cd $$dir && $(MAKE) clean && cd ..; done"
+
 
 
 #Dir.chdir(dir)
